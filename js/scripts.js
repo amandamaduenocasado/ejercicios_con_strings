ccd1c6f97073a5d7db3df2d5454efa3e451fa878
Andrea <amandacaso97@gmail.com>
2024-10-18 14:55:19 +0200

    actualizacion

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+function transformWord(word) {
+  if (word.length > 5) {
+    console.log(word.toUpperCase());
+  } else {
+    console.log(word.toLowerCase());
+  }
+}
+
+transformWord('Hi');
+transformWord('Sweetie');
+
+function randomWord(word) {
+  console.log(, Math.random());
+}
+
+randomWord('persimon');
